b499f80c7ca9659c83d9e31b642e28f7e50a3321
ninglei <lei.ning@jrj.com.cn>
2016-04-14 20:30:44 +0800

    test log

--- src/main/java/arubtsov/controller/TestWebController.java
+++ src/main/java/arubtsov/controller/TestWebController.java
@@ -1,5 +1,7 @@
 package arubtsov.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,23 +15,26 @@ public class TestWebController {
 
 	@Autowired
 	private RedisService redisService;
-	
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping(value = "/echo", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 	public String echo() {
+		logger.info("Hello init Docker!!!!!!!!!!!!!!!!!!!");
 		return "Hello init Docker!!!!!!!!!!!!!!!!!!!";
 	}
 
 	@RequestMapping(value = "/redis/get/{key}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 	public String rGet(@PathVariable("key") String key) {
 		String vString = redisService.get(key);
-		System.out.println("redisService:" + vString);
+		logger.info("redisService: {}", vString);
 		return "Redis Value:" + vString;
 	}
 
 	@RequestMapping(value = "/redis/set/{key}/{value}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 	public String rSet(@PathVariable("key") String key, @PathVariable("value") String value) {
 		redisService.set(key, value);
-		System.out.println("redisService:  SET OK");
+		logger.info("redisService:  key : {} ,value : {} SET OK", key, value);
 		return "Redis Value: SET OK key:" + key + ",value:" + value;
 	}
 
